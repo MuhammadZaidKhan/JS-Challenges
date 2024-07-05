0a3a6bc23e57e7cf8a8bdc513251d2955880ac78
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-07-05 11:55:26 +0500

    Practicing Object and arrays

---
+++ ob.js
@@ -0,0 +1,40 @@
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+
+// const obj3 = { obj1, obj2 };
+
+// const obj3 = Object.assign(obj1, obj2);
+
+// const obj3 = { ...obj1, ...obj2 };
+
+console.log(Object.values(obj2));
+
+// const ims = new Object();
+// const ims2 = {
+//   username: {
+//     fullname: {
+//       fistname: "Institute of",
+//       lastname: "Managment Sciences",
+//     },
+//   },
+// };
+
+// ims2.name = "IMSciences";
+// ims2.age = 30;
+
+// console.log(ims2.username.fullname.fistname);
+// console.log(ims);
+
+// const sym = Symbol("key1");
+
+// const newObj = {
+//   [sym]: "abc",
+//   me: "zaid",
+// };
+
+// console.log(...newObj);
+
+// const array1 = [1, 2, 3, { me: "zaid" }];
+// const array2 = [4, 5, 6];
+
+// console.log(array1.concat(array2));
