d375fc5edf556345cf11fb28a4b35f6d156917c1
MuhammadZaidKhan <write2zaidkhan@gmail.com>
2024-07-16 17:41:30 +0500

    Some practice

---
+++ foreach.js
@@ -0,0 +1,27 @@
+// const arr = [
+//   {
+//     name: "zaid",
+//     age: 20,
+//   },
+//   {
+//     name: "ali",
+//     age: 30,
+//   },
+//   {
+//     name: "waqas",
+//     age: 40,
+//   },
+//   {
+//     name: "adnan",
+//     age: 50,
+//   },
+// ];
+
+// arr.forEach((item) => {
+//   console.log(item.name);
+// });
+
+const num = [1, 2, 3];
+
+let num2 = num.filter((item) => item > 1);
+console.log(num2);
